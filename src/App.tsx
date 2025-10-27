c0c219b0a019a80343a11201f68f5aa38e02d4c0
Dasha <girl.is.anime20@gmail.com>
2025-10-27 17:07:58 +0300

    feat: add configureStore

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import FavoritesPage from './pages/FavoritesPage/favoritesPage';
 import OfferPage from './pages/OfferPage/offerPage';
 import PrivateRoute from './components/PrivateRoute/privateRoute';
 import { offers } from './mocks/offers';
+import { Provider } from 'react-redux';
+import { store } from './components/Store';
 
 type AppProps = {
   mainPageData: MainPageProps;
@@ -17,17 +19,19 @@ const App: FC<AppProps> = ({mainPageData}) => {
   const isAuthorized = false;
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage {...mainPageData} />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route element={<PrivateRoute isAuthorized={isAuthorized} />}>
-          <Route path="/favorites" element={<FavoritesPage offers={offers} />} />
-        </Route>
-        <Route path="/offer/:id" element={<OfferPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage {...mainPageData} />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<PrivateRoute isAuthorized={isAuthorized} />}>
+            <Route path="/favorites" element={<FavoritesPage offers={offers} />} />
+          </Route>
+          <Route path="/offer/:id" element={<OfferPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
