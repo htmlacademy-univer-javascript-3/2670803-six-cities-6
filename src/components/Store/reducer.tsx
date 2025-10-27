db527d86a954eb688a9877192af0a7582c6bfeaf
Dasha <girl.is.anime20@gmail.com>
2025-10-27 16:42:31 +0300

    feat: add reducer

---
+++ src/components/Store/reducer.tsx
@@ -0,0 +1,34 @@
+import { Offer } from '../../mocks/types/offer';
+
+export type StateType = {
+  city: string;
+  offers: Offer[];
+};
+
+export const initialState: StateType = {
+  city: 'Amsterdam',
+  offers: [],
+};
+
+type SetCityAction = {
+  type: 'SET_CITY';
+  payload: string;
+};
+
+type SetOffersAction = {
+  type: 'SET_OFFERS';
+  payload: Offer[];
+};
+
+export type ActionType = SetCityAction | SetOffersAction;
+
+export const reducer = (state: StateType, action: ActionType): StateType => {
+  switch (action.type) {
+    case 'SET_CITY':
+      return { ...state, city: action.payload };
+    case 'SET_OFFERS':
+      return { ...state, offers: action.payload };
+    default:
+      return state;
+  }
+};
