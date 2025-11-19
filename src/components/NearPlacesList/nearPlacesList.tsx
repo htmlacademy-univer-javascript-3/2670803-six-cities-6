fd1cda572f54f76a9f82902a13199202d5a967f3
Dasha <girl.is.anime20@gmail.com>
2025-11-19 22:34:43 +0300

    fix: fixed nearPlaceList props

--- src/components/NearPlacesList/nearPlacesList.tsx
+++ src/components/NearPlacesList/nearPlacesList.tsx
@@ -4,14 +4,17 @@ import PlaceCard from '../PlaceCard/placeCard';
 
 type NearPlacesListProps = {
   offers: Offer[];
+  onOfferHover?: (offerId: string | null) => void;
 }
 
-const NearPlacesList: FC<NearPlacesListProps> = ({ offers }) => (
+const NearPlacesList: FC<NearPlacesListProps> = ({ offers, onOfferHover }) => (
   <div className="near-places__list places__list">
     {offers.map((offer) => (
       <PlaceCard
         key={offer.id}
         offer={offer}
+        onMouseEnter={() => onOfferHover?.(offer.id)}
+        onMouseLeave={() => onOfferHover?.(null)}
       />
     ))}
   </div>
